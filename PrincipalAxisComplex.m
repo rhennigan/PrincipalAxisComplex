48ef42ac2526406d9994e91973d6895c187d0a5e
rhennigan <rhennigan@gmail.com>
2015-05-01 16:07:22 -0400

    Initial commit

---
+++ PrincipalAxisComplex.m
@@ -0,0 +1,22 @@
+(* Mathematica Package         *)
+(* Created by IntelliJ IDEA    *)
+
+(* :Title: PrincipalAxisComplex     *)
+(* :Context: PrincipalAxisComplex`  *)
+(* :Author: rhennigan            *)
+(* :Date: 5/1/2015              *)
+
+(* :Package Version: 1.0       *)
+(* :Mathematica Version:       *)
+(* :Copyright: (c) 2015 rhennigan *)
+(* :Keywords:                  *)
+(* :Discussion:                *)
+
+BeginPackage["PrincipalAxisComplex`"]
+(* Exported symbols added here with SymbolName::usage *)
+
+Begin["`Private`"] (* Begin Private Context *)
+
+End[] (* End Private Context *)
+
+EndPackage[]
